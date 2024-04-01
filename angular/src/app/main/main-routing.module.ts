729492fb74b38306717cca2c7a0b490970864636
= <nhanhoa21012002@gmail.com>
2024-04-01 08:34:50 +0700

    add backend repo

--- angular/src/app/main/main-routing.module.ts
+++ angular/src/app/main/main-routing.module.ts
@@ -2,22 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
-    imports: [
-        RouterModule.forChild([
-            {
-                path: '',
-                children: [
-                    {
-                        path: 'dashboard',
-                        loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
-                        data: { permission: 'Pages.Tenant.Dashboard' },
-                    },
-                    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-                    { path: '**', redirectTo: 'dashboard' },
-                ],
-            },
-        ]),
-    ],
-    exports: [RouterModule],
+  imports: [
+    RouterModule.forChild([
+      {
+        path: '',
+        children: [
+          {
+            path: 'dashboard',
+            loadChildren: () => import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+            data: { permission: 'Pages.Tenant.Dashboard' },
+          },
+          {
+            path: 'classroom',
+            loadChildren: () => import('./classroom/classroom.module').then((m) => m.ClassroomModule),
+            data: { permission: 'Pages.Tenant.Classroom' },
+          },
+          { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+          { path: '**', redirectTo: 'dashboard' },
+        ],
+      },
+    ]),
+  ],
+  exports: [RouterModule],
 })
-export class MainRoutingModule {}
+export class MainRoutingModule { }
